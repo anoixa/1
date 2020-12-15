f4929f436dcc969fa64792eaf09ff1ed9711d6b7
anoixa <1692459756@qq.com>
2020-12-15 17:52:55 +0800

    22

--- demo.c
+++ demo.c
@@ -7,9 +7,13 @@
 #define N 10
 
 void mainMenu();//总主菜单
+void mainMenuAction();
 void fzmainMenu();//分支部分菜单函数
+void fzmainMenuAction();
 void xhmainMenu();//循环部分菜单函数
+void xhmainMenuAction();
 void szmainMenu();//数组部分菜单函数
+void szmainMenuAction();
 
 //分支部分函数声明
 void fz1();
@@ -42,15 +46,16 @@ void sz8();
 /***************************/
 int main()   //main 函数
 {
-    mainMenu();
+    mainMenuAction();
 
     return 0;
-    system("pause");
 }
 
 /***************************/
 
-void mainMenu()  //总主菜单
+//菜单内容区
+/*************************************/
+void mainMenu()//总主菜单内容
 {
     printf( "--------------------\n");
     printf( "|  学习成果展示系统 |\n");
@@ -61,47 +66,8 @@ void mainMenu()  //总主菜单
     printf( "--------------------\n");
     printf( "|  0 退 出  系 统   |\n");
     printf( "--------------------\n");
-    while(1)
-    {
-        int n;
-        printf("请选择(0-3):");
-        scanf("%d", &n);
-        if( n==1 )
-        {
-            system("cls");
-            fzmainMenu();
-            break;
-        }
-        else if( n==2 )
-        {
-            system("cls");
-            xhmainMenu();
-            break;
-        }
-        else if( n==3 )
-        {
-            system("cls");
-            szmainMenu();
-            break;
-        }
-        else if( n==0 )
-        {
-            exit(0);
-        }
-        else
-        {
-            printf("没有这项功能。\n");
-			printf("请核对后重新选择\n");
-            system("pause");
-            system("cls");
-            mainMenu();
-        }
-    }
 }
-
-
-/*****************************************/
-void fzmainMenu() //分支主菜单
+void fzmainMenu()//分支主菜单内容
 {
     printf( "------------------------\n");
     printf( "|  分支结构部分程序     |\n");
@@ -115,6 +81,91 @@ void fzmainMenu() //分支主菜单
     printf( "------------------------\n");
     printf( "|  0 返回主菜单         |\n");
     printf( "------------------------\n");
+}
+void xhmainMenu()//循环主菜单内容
+{
+    printf( "--------------------\n");
+    printf( "|  循环结构部分程序 |\n");
+    printf( "--------------------\n");
+    printf( "|  1 判断完数      |\n");
+    printf( "|  2 求反序数      |\n"); 
+    printf( "|  3 存款利息      |\n");
+    printf( "|  4 判断素数      |\n");
+    printf( "|  5 水仙花数      |\n");
+    printf( "|  6 n的阶乘       |\n");
+    printf( "|  7 金字塔        |\n");
+    printf( "|  8 同构数        |\n");
+    printf( "--------------------\n");
+    printf( "|  0 返回主菜单     |\n");
+    printf( "--------------------\n");
+}
+void szmainMenu()//数组主菜单内容
+{
+    printf( "----------------------\n");
+    printf( "|   数组结构部分程序  |\n");
+    printf( "----------------------\n");
+    printf( "|  1 逆序输出数组     |\n");
+    printf( "|  2 累加求和         |\n"); 
+    printf( "|  3 字符串长度       |\n");
+    printf( "|  4 统计字母个数     |\n");
+    printf( "|  5 变换字符         |\n");
+    printf( "|  6 删除字符         |\n");
+    printf( "|  7 统计字符         |\n");
+    printf( "|  8 判断回文         |\n");
+    printf("----------------------\n");
+    printf( "|  0 返回主菜单       |\n");
+    printf("----------------------\n");
+}
+/*************************************/
+
+void mainMenuAction()  //总主菜单
+{
+    mainMenu();
+    while(1)
+    {
+        int n;
+        printf("请选择(0-3):");
+        scanf("%d", &n);
+        if( n==1 )
+        {
+            system("cls");
+            fzmainMenuAction();
+            break;
+        }
+        else if( n==2 )
+        {
+            system("cls");
+            xhmainMenuAction();
+            break;
+        }
+        else if( n==3 )
+        {
+            system("cls");
+            szmainMenuAction();
+            break;
+        }
+        else if( n==0 )
+        {
+            exit(0);
+        }
+        else
+        {
+            printf("没有这项功能。\n");
+			printf("请核对后重新选择\n");
+            printf("请按任意键继续...\n");
+            getch();
+            system("cls");
+            mainMenuAction();
+        }
+    }
+}
+
+//分菜单区
+/*****************************************/
+void fzmainMenuAction() //分支主菜单
+{
+    getchar();
+    fzmainMenu();
     while(1)
     {
         int a1;
@@ -135,36 +186,23 @@ void fzmainMenu() //分支主菜单
         else if(a1 == 0)
         {
             system("cls");
-            mainMenu();
+            mainMenuAction();
             break;
         }
         else
         {
             printf("没有这项功能。\n");
 			printf("请核对后重新选择\n");
-            system("pause");
+            printf("请按任意键继续...\n");
+            getch();
             system("cls");
-            fzmainMenu();
+            fzmainMenuAction();
         }
     }
 }
-
-void xhmainMenu() //循环主菜单
+void xhmainMenuAction() //循环主菜单
 {
-    printf( "--------------------\n");
-    printf( "|  循环结构部分程序 |\n");
-    printf( "--------------------\n");
-    printf( "|  1 判断完数      |\n");
-    printf( "|  2 求反序数      |\n"); 
-    printf( "|  3 存款利息      |\n");
-    printf( "|  4 判断素数      |\n");
-    printf( "|  5 水仙花数      |\n");
-    printf( "|  6 n的阶乘       |\n");
-    printf( "|  7 分支程序      |\n");
-    printf( "|  8 分支程序      |\n");
-    printf( "--------------------\n");
-    printf( "|  0 返回主菜单     |\n");
-    printf( "--------------------\n");
+    xhmainMenu();
     while(1)
     {
         int a2;
@@ -187,36 +225,23 @@ void xhmainMenu() //循环主菜单
         else if(a2 == 0)
         {
             system("cls");
-            mainMenu();
+            mainMenuAction();
             break;
         }
         else
         {
             printf("没有这项功能。\n");
 			printf("请核对后重新选择\n");
-            system("pause");
+            printf("请按任意键继续...\n");
+            getch();
             system("cls");
-            xhmainMenu();
+            xhmainMenuAction();
         }
     }
 }
-
-void szmainMenu() //循环主菜单
+void szmainMenuAction() //循环主菜单
 {
-    printf( "--------------------\n");
-    printf( "|  数组结构部分程序 |\n");
-    printf( "--------------------\n");
-    printf( "|  1 分支程序1     |\n");
-    printf( "|  2 分支程序2     |\n"); 
-    printf( "|  3 分支程序3     |\n");
-    printf( "|  4 分支程序4     |\n");
-    printf( "|  5 分支程序5     |\n");
-    printf( "|  6 分支程序6     |\n");
-    printf( "|  7 分支程序7     |\n");
-    printf( "|  8 分支程序8     |\n");
-    printf( "--------------------\n");
-    printf( "|  0 返回 菜单     |\n");
-    printf( "--------------------\n");
+    szmainMenu();
     while(1)
     {
         int a3;
@@ -239,23 +264,24 @@ void szmainMenu() //循环主菜单
         else if(a3 == 0)
         {
             system("cls");
-            mainMenu();
+            mainMenuAction();
             break;
         }
         else
         {
             printf("没有这项功能。\n");
 			printf("请核对后重新选择\n");
-            system("pause");
+            printf("请按任意键继续...\n");
+            getch();
             system("cls");
-            szmainMenu();
+            szmainMenuAction();
         }
     }
 }
 /*****************************************/
 
 
-
+/*****************************************/   //子函数区
 //此为分支函数区
 /**********************************/
 void fz1()//及格了吗？
@@ -275,7 +301,7 @@ void fz1()//及格了吗？
     printf("\n");
     system("pause");
     system("cls");
-    fzmainMenu();
+    fzmainMenuAction();
 
 }
 void fz2()//求绝对值
@@ -300,7 +326,7 @@ void fz2()//求绝对值
     printf("\n");
     system("pause");
     system("cls");
-    fzmainMenu();
+    fzmainMenuAction();
 
 }
 void fz3()//比较两数中最大
@@ -320,7 +346,7 @@ void fz3()//比较两数中最大
     printf("\n");
     system("pause");
     system("cls");
-    fzmainMenu();
+    fzmainMenuAction();
     
 }
 void fz4()//判断闰年
@@ -340,7 +366,7 @@ void fz4()//判断闰年
     printf("\n");
     system("pause");
     system("cls");
-    fzmainMenu();
+    fzmainMenuAction();
 
 }
 void fz5()//三角形面积
@@ -362,7 +388,7 @@ void fz5()//三角形面积
     printf("\n");
     system("pause");
     system("cls");
-    fzmainMenu();
+    fzmainMenuAction();
 
 }
 void fz6()//三角形类型
@@ -371,7 +397,7 @@ void fz6()//三角形类型
     printf("请输入长宽高:");
     double a,b,c;
     scanf("%lf%lf%lf",&a,&b,&c);
-    if(a>0&&b>0&&c>0&&a+b>c&&a+c>b&&b+c>a)
+    if(a>0 && b>0 && c>0 && a+b>c && a+c>b && b+c>a)
     {
         if(a==b&&b==c&&a==c)
         {
@@ -393,7 +419,7 @@ void fz6()//三角形类型
     printf("\n");
     system("pause");
     system("cls");
-    fzmainMenu();
+    fzmainMenuAction();
 
 }
 /**********************************/
@@ -425,7 +451,7 @@ void xh1()//判断完数
     printf("\n");
     system("pause");
     system("cls");
-    xhmainMenu();
+    xhmainMenuAction();
     
 }
 void xh2()//求反序数
@@ -445,7 +471,7 @@ void xh2()//求反序数
     printf("\n");
     system("pause");
     system("cls");
-    xhmainMenu();
+    xhmainMenuAction();
 
 }
 void xh3()//存款利息
@@ -467,7 +493,7 @@ void xh3()//存款利息
     printf("\n");
     system("pause");
     system("cls");
-    xhmainMenu();
+    xhmainMenuAction();
 
 }
 void xh4()//判断素数
@@ -493,7 +519,7 @@ void xh4()//判断素数
     printf("\n");
     system("pause");
     system("cls");
-    xhmainMenu();
+    xhmainMenuAction();
 
 }
 void xh5()//水仙花数
@@ -517,55 +543,302 @@ void xh5()//水仙花数
     printf("\n");
     system("pause");
     system("cls");
-    xhmainMenu();
+    xhmainMenuAction();
 
 }
 void xh6()//n的阶乘
 {
+    printf("你正在调用函数6\n");
+    printf("请输入一个数:");
+    int n,a;
+	int i,j=1;
+	scanf("%d",&n);
+    if( n != 0 )
+    {
+        for(i=1;i<=n;i++)
+        {
+		    if(n != 0)
+		    {
+			    j = j*i;
+			    a = j;
+		    }
+		    else
+		    {
+			    a = 1;
+			    break;
+		    }
+        }
+    }
+    else
+    {
+        a = 1;
+    }
+	printf("结果:%d",a);
+    printf("\n");
+    system("pause");
+    system("cls");
+    xhmainMenuAction();
+	
+}
+void xh7()//金字塔
+{
+    printf("你正在调用函数7\n");
+    printf("请输入行数:");
+    int i,j,k;
+    int n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(k=i;k<=n-1;k++)
+            printf(" ");
+        for(j=1;j<+2*i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    system("pause");
+    system("cls");
+    xhmainMenuAction();
 
 }
-void xh7()
+void xh8()//同构数
 {
-
-}
-void xh8()
-{
+    printf("你正在调用函数8\n");
+    printf("请输入一个数:");
+    long int m,m1,n,a,b;
+    scanf("%ld",&m);
+    m1=m;
+    n=m*m1;
+    while(m1)
+    { 
+        a=m1%10; b=n%10;
+        if(a!=b)
+        {
+            break;
+        }
+        m1=m1/10; 
+        n=n/10;
+    }
+    if(m1!=0) 
+    {
+        printf("no\n");
+    }
+    else 
+    {
+        printf("yes\n");
+    } 
+    printf("\n");
+    system("pause");
+    system("cls");
+    xhmainMenuAction();
 
 }
 /**********************************/
 
 //此为数组函数区
 /**********************************/
-void sz1()
+void sz1()//数组倒序
 {
     printf("你正在调用函数1\n");
+    printf("请输入10个数:\n");
+    int a[10], i;
+    for(i=0; i<10; i++)//输入数组所有元素
+    {
+        scanf("%d",&a[i]) ;
+    }
+    for(i=9; i>=0;  i--)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+    system("pause");
+    system("cls");
+    szmainMenuAction();
+
 }
-void sz2()
+void sz2()//数组求和
 {
     printf("你正在调用函数2\n");
+    printf("请输入10个数:\n");
+    int a[10],i,s;
+    s=0;
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+        s+=a[i];
+    }
+    printf("和为:%d\n",s);
+    printf("\n");
+    system("pause");
+    system("cls");
+    szmainMenuAction();
+
 }
-void sz3()
+void sz3()//数组长度
 {
     printf("你正在调用函数3\n");
-}
-void sz4()
+    printf("请输入字符串:\n");
+    char string[100];
+    int sum=0;
+    int i;
+    getchar();
+    gets(string);
+    for (i=0; i<100&&string[i]!='\0'; i++) 
+    {
+        sum = sum + 1;
+    }
+    printf("字符串长度为:%d\n",i);
+    printf("\n");
+    system("pause");
+    system("cls");
+    szmainMenuAction();
+} 
+void sz4()//统计字母个数
 {
     printf("你正在调用函数4\n");
+    printf("请输入字符串:\n");
+    char s[10000];
+    int i,zm=0;
+    getchar();
+    gets(s);
+    for(i=0;s[i];i++)
+    {
+        if(s[i]>='a'&&s[i]<='z'||s[i]>='A'&&s[i]<='Z')
+        {
+            zm++;
+        }
+    }
+    printf("字母个数为:%d\n",zm);
+    printf("\n");
+    system("pause");
+    system("cls");
+    szmainMenuAction();
+
 }
-void sz5()
+void sz5()//变换字符
 { 
     printf("你正在调用函数5\n");
+    printf("请输入字符串:\n");
+    char s[10000],ch;
+    int i;
+    getchar();
+    gets(s);
+    scanf("%c",&ch);
+    for(i=0;s[i];i++)
+    {
+        if(s[i]==ch)
+        {
+            s[i] -= 32;
+        }
+    }
+    printf("新的字符串为\n");
+    puts(s);
+    printf("\n");
+    system("pause");
+    system("cls");
+    szmainMenuAction();
+    
 }
 void sz6()
 {
     printf("你正在调用函数6\n");
+    printf("请输入字符串:\n");
+    char s[10000],ch;
+    int i,j=0;
+    getchar();
+    gets(s);
+    scanf("%c",&ch);
+    for(i=0;s[i];i++)
+    {
+        if(s[i]!=ch)
+        {
+            s[j++]=s[i];
+        }
+        s[j]='\0';
+    }
+    printf("新的字符串为\n");
+    puts(s);
+    printf("\n");
+    system("pause");
+    system("cls");
+    szmainMenuAction();
+
 }
 void sz7()
 {
     printf("你正在调用函数7\n");
+    printf("请输入字符串:\n");
+    char s[200];
+    int i;
+    int dx=0,xx=0,sz=0,kg=0,qt=0;
+    getchar();
+    gets(s);
+    for(i=0; s[i] != '\0'; i++)
+    {
+        if( s[i]>='A' && s[i]<='Z')
+        {
+            dx++;
+        }
+        else if(s[i]>='a' && s[i]<='z')
+        {
+            xx++;
+        }
+        else if(s[i]>='0' && s[i]<='9')
+        {
+            sz++;
+        }
+        else if( ' '== s[i])
+        {
+            kg++;
+        }
+        else
+        {
+            qt++;
+        }
+    }
+    printf("大写:%d 小写:%d 数字:%d 空格:%d 其他:%d\n", dx, xx, sz, kg, qt);
+    printf("\n");
+    system("pause");
+    system("cls");
+    szmainMenuAction();
+
 }
 void sz8()
 {
     printf("你正在调用函数8\n");
+    printf("请输入字符串:\n");
+    char s[200];
+    int i,j,flag=1;
+    getchar();
+    gets( s );
+    for(j=0;s[j];j++);
+    j--;
+    for(i=0;i<j;i++,j--)
+    {
+        if(s[i]!=s[j])
+        {
+            flag=0;
+            break; 
+        }
+    }
+    if(flag==1)
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
+    printf("\n");
+    system("pause");
+    system("cls");
+    szmainMenuAction();
+
 }
 /**********************************/
+/*****************************************/
